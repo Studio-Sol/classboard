ac36ab7d6abdfb38bc64ee097bcffa87d23f02b5
sol-studio <cloud8862@gmail.com>
2022-04-15 15:17:55 +0000

    파일정리

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-hi
+beta test
--- modules/jwt.js
+++ modules/jwt.js
@@ -6,7 +6,6 @@ const options = require('../config/jwt').options;
 module.exports = {
     sign: async (payload) => {
         const result = {
-            //sign메소드를 통해 access token 발급!
             token: jwt.sign(payload, secretKey, options),
             refreshToken: UID.sync(256)
         };
@@ -15,7 +14,6 @@ module.exports = {
     verify: async (token) => {
         let decoded;
         try {
-            // verify를 통해 값 decode!
             decoded = jwt.verify(token, secretKey);
         } catch (err) {
             if (err.message === 'jwt expired') {
