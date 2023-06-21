f21441942d025ae5cf1987228420c5478ff8c74d
sol-studio <cloud8862@gmail.com>
2023-06-21 18:22:43 +0900

    classboard.kr

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,21 @@
-const startAt = new Date().getUTCMilliseconds();
 import loaders from "./loaders";
 import express from "express";
 import http from "http";
-import request from "request";
-import { Command } from "commander";
 
 (async () => {
     const app = express();
     const httpServer = new http.Server(app);
-    const allow_hosts = ["redirect.kro.kr", "127.0.0.1"];
+    const allow_hosts = [
+        "https://classboard.kr",
+        "redirect.kro.kr",
+        "127.0.0.1",
+    ];
     const inspecting = false;
-    const serviceURL = "http://redirect.kro.kr";
+    const serviceURL = "https://classboard.kr";
 
     await loaders(app, allow_hosts, inspecting, serviceURL);
 
-    const program = new Command();
-    program
-        .option("-p, --port <number>", "server port", process.env.PORT)
-        .option("-h, --host <host>", "server host", process.env.HOST)
-        .parse();
-
-    httpServer.listen(
-        parseInt(program.opts().port),
-        program.opts().host,
-        () => {
-            console.log("listening...", program.opts().port);
-        }
-    );
+    httpServer.listen(3000, "0.0.0.0", () => {
+        console.log("listening...", 3000);
+    });
 })();
