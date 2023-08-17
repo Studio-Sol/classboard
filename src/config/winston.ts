dd7f9794a1f457d3099e4c90f4b60c20f73ad110
sol-studio <cloud8862@gmail.com>
2023-08-17 23:35:21 +0900

    graylog

--- src/config/winston.ts
+++ src/config/winston.ts
@@ -1,55 +1,21 @@
 import winston from "winston";
-import winstonDaily from "winston-daily-rotate-file";
+import WinstonGraylog2 from "@eximius/winston-graylog2";
 
-const logDir = "logs"; // logs 디렉토리 하위에 로그 파일 저장
-const { combine, timestamp, printf } = winston.format;
-
-// Define log format
-const logFormat = printf((info) => {
-    return `${info.timestamp} ${info.level}: ${info.message}`;
-});
-
-/*
- * Log Level
- * error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
- */
 const logger = winston.createLogger({
-    format: combine(
-        timestamp({
-            format: "YYYY-MM-DD HH:mm:ss",
-        }),
-        logFormat
-    ),
+    format: winston.format.json(),
     transports: [
-        // info 레벨 로그를 저장할 파일 설정
-        new winstonDaily({
-            level: "info",
-            datePattern: "YYYY-MM-DD",
-            dirname: logDir,
-            filename: `%DATE%.log`,
-            maxFiles: 30, // 30일치 로그 파일 저장
-            zippedArchive: true,
-        }),
-        // error 레벨 로그를 저장할 파일 설정
-        new winstonDaily({
-            level: "error",
-            datePattern: "YYYY-MM-DD",
-            dirname: logDir + "/error", // error.log 파일은 /logs/error 하위에 저장
-            filename: `%DATE%.error.log`,
-            maxFiles: 30,
-            zippedArchive: true,
+        new winston.transports.Console(),
+        new WinstonGraylog2({
+            name: "classboard",
+            level: "info", // 로그 레벨 설정
+            graylog: {
+                servers: [{ host: "127.0.0.1", port: 12201 }],
+                facility: "classboard",
+            },
         }),
     ],
 });
 
-logger.add(
-    new winston.transports.Console({
-        format: winston.format.combine(
-            winston.format.colorize(), // 색깔 넣어서 출력
-            winston.format.simple() // `${info.level}: ${info.message} JSON.stringify({ ...rest })` 포맷으로 출력
-        ),
-    })
-);
 const stream = {
     // morgan wiston 설정
     write: (message) => {
