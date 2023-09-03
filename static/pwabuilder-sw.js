23a37824a0fdf81955702ca55fd3165f30fe429e
sol-studio <cloud8862@gmail.com>
2023-09-03 18:13:36 +0900

    delete cache

--- static/pwabuilder-sw.js
+++ static/pwabuilder-sw.js
@@ -16,3 +16,14 @@ workbox.routing.registerRoute(
         cacheName: CACHE,
     })
 );
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
