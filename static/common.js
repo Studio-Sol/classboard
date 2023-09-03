9326470ee4e7be0bf795687aae8b534644b46334
sol-studio <cloud8862@gmail.com>
2023-09-03 17:52:12 +0900

    경량화

--- static/common.js
+++ static/common.js
@@ -61,7 +61,7 @@ window.addEventListener("load", () => {
                 offlineMessageBox.style.zIndex = "10000000000";
                 offlineMessageBox.id = "offlineMessageBox";
 
-                offlineMessageBox.innerHTML = `<h1>오프라인 모드입니다</h1><i onclick="document.getElementById('offlineMessageBox').remove();localStorage.setItem('showOfflineMessage','false')" style="font-size: 2rem" class="pe-3 fa fa-close"></i>`;
+                offlineMessageBox.innerHTML = `<h1>오프라인 모드입니다</h1><svg onclick="document.getElementById('offlineMessageBox').remove();localStorage.setItem('showOfflineMessage','false')" style="font-size: 2rem" class="pe-3" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>`;
                 document.body.append(offlineMessageBox);
             }
         }
